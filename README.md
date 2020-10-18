95654eb815f19a8887363d33fe5c79a72d0420eb
Ray C Horn <raychorn@gmail.com>
2020-10-17 18:17:36 -0600

    working on the multi tree case.

--- README.md
+++ README.md
@@ -2,22 +2,42 @@
 
 ## Table of Contents
 
+- [Preface](#preface)
 - [About](#about)
 - [Getting Started](#getting_started)
 - [Usage](#usage)
 - [Contributing](../CONTRIBUTING.md)
 
+## Preface <a name = "preface"></a>
+
+I am not sure why this problem exists other than the folks at Hacker Rank who probably spend all their time dreaming this stuff up might have thought this was
+interesting.
+
+The only part of this problem that seems the least bit interesting to me is the obvious programming effort required to make logical sense of the really crappy way
+the data was architected.  Two arrays to describe a set of trees?  Really?!?  Who would dream this up?  I cannot say anything charitable about any of this but this
+was the problem as best I could recall.  I am sure Hacker Rank has more of these problems than anyone might be able to document and I am sure none of it makes
+any real sense.
+
+And what's the deal about putting an artificial time limit on coding the problem?  When does this occur in real life?  Never?  I have been working in the industry for
+longer than I like to recall and NEVER have I ever been faced with getting only 30 minutes to get code written to resolve any issue.  
+
+Ok.  Let's assume you can buy into the problem.  What follows here was my take on it.  Your mileage may vary.
+
 ## About <a name = "about"></a>
 
-Write about 1-2 paragraphs describing the purpose of your project.
+Consider two arrays as parent {-1, 0, 1, 2, -1} and sizes {1, 4, 3, 4, 1} where each node of the tree has a pointer to another node with the base of the tree as -1 and
+each node has a size. Make the trees, however many as may exist, be as balanced as possible and return the difference between the minimum and maximum as a positive integer.
+
+You may cut one edge to balance the trees. (I am a bit fuzzy as to this part of the problem statement.)  You can cut any edge but once but the edge is gone?  (Again I am fuzzy and dubious
+about this too.)
 
 ## Getting Started <a name = "getting_started"></a>
 
-These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See [deployment](#deployment) for notes on how to deploy the project on a live system.
+Checkout the code and have fun.
 
 ### Prerequisites
 
-What things you need to install the software and how to install them.
+Pay me if you need help. Seriously, I need the cash. Otherwise you're on your own.
 
 ```
 Give examples
@@ -25,9 +45,7 @@ Give examples
 
 ### Installing
 
-A step by step series of examples that tell you how to get a development env running.
-
-Say what the step will be
+Go figure.  Really. It's GO.  Or it's Go time. So Go.
 
 ```
 Give the example
@@ -39,10 +57,11 @@ And repeat
 until finished
 ```
 
-End with an example of getting some data out of the system or using it for a little demo.
+Make it work.
 
 ## Usage <a name = "usage"></a>
 
-Add notes about how to use the system.
+Use it any way you wish.
 
+Yeah, there a License and a Copyright notice.  Deal with it.
 (c). Copyright Ray C Horn, All Rights Reserved.
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 /*
@@ -19,8 +20,22 @@ type node struct {
 	visited bool
 }
 
+type nodes []node
+
 func (n *node) asString() string {
 	return fmt.Sprintf("{parent:%d, size:%d, visited:%t}", n.parent, n.size, n.visited)
+}
+
+func (s nodes) Len() int {
+	return len(s)
+}
+
+func (s nodes) Less(i, j int) bool {
+	return s[i].parent < s[j].parent
+}
+
+func (s nodes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
 }
 
 func dumpArray(items []int32) string {
@@ -51,6 +66,20 @@ func dumpTree(tree map[int32]node) string {
 	return output
 }
 
+func unpackTree(tree map[int32]node) nodes {
+	theNodes := nodes{}
+	for _,v := range tree {
+		theNodes = append(theNodes, v)
+	}
+	return theNodes
+}
+
+/*
+func sortTree(tree map[int32]node) []node {
+
+}
+*/
+
 func isNodeInAnyTree(trees []map[int32]node, ptr int32) bool {
 	hasPtr := false
 
@@ -62,6 +91,14 @@ func isNodeInAnyTree(trees []map[int32]node, ptr int32) bool {
 		}
 	}
 	return hasPtr
+}
+
+func pluralize(val interface{}) string {
+	plural := ""
+	if (len(val) > 1) {
+		plural = "s"
+	}
+	return plural
 }
 
 func mostBalancedPartition(parent []int32, filesSize []int32) (int32, string) {
@@ -87,7 +124,10 @@ func mostBalancedPartition(parent []int32, filesSize []int32) (int32, string) {
 				}
 				scores[len(trees)] += filesSize[iPtr]
 				if (parent[iPtr] == -1) {
+					fmt.Printf("(1) Store tree and break. iPtr=%d, parent[%d]=%d\n", iPtr, iPtr, parent[iPtr])
 					trees = append(trees, tree)
+					tree = make(map[int32]node)
+					break
 				} else {
 					iPtr = parent[iPtr]
 					if (iPtr == 0) || (iPtr == -1) {
@@ -98,7 +138,8 @@ func mostBalancedPartition(parent []int32, filesSize []int32) (int32, string) {
 						}
 						scores[len(trees)] += filesSize[iPtr]
 						trees = append(trees, tree)
-						fmt.Println("break")
+						tree = make(map[int32]node)
+						fmt.Println("(2) Store tree and break.")
 						break
 					}
 				}
@@ -124,20 +165,41 @@ func mostBalancedPartition(parent []int32, filesSize []int32) (int32, string) {
 		fmt.Println()
 	}
 
+	theTrees := []nodes{}
 	fmt.Printf("BEGIN: trees\n")
 	for ii,vv := range trees {
 		fmt.Printf("%d -> %s (%d)\n", ii, dumpTree(vv), scores[ii])
+		theTrees = append(theTrees, unpackTree(vv))
 	}
 	fmt.Printf("END!!!\n")
 	fmt.Println()
 
-	return 0, output
+	response := 0
+	if (len(trees) == 1) {
+		fmt.Println("Only one tree.")
+		response = int(scores[0])
+	} else {
+		// here we determine which edge to remove in the tree with the largest size to make the trees as equal as possible. Find the min difference.
+		plural := pluralize(theTrees)
+		fmt.Printf("Consider %d tree%s.", len(theTrees), plural)
+		for i,someNodes := range theTrees {
+			fmt.Println(someNodes)
+			fmt.Printf("(1) #%d Is Sorted %t \n", i, sort.IsSorted(someNodes))
+			sort.Sort(someNodes)
+			fmt.Println(someNodes)
+			fmt.Printf("(2) #%d Is Sorted %t \n", i, sort.IsSorted(someNodes))
+			fmt.Println()
+		}
+	}
+
+	return int32(response), output
 }
 
 func main() {
 	var datasets = [][][]int32{
-		{{-1, 0, 1, 2}, {1, 4, 3, 4}}, 
-		{{-1, 0, 0, 0}, {10, 11, 10, 10}}, 
+		{{-1, 0, 1, 2, -1}, {1, 4, 3, 4, 1}}, // two trees
+		{{-1, 0, 1, 2}, {1, 4, 3, 4}},   // one tree
+		{{-1, 0, 0, 0}, {10, 11, 10, 10}}, // three trees
 		{{-1, 0}, {20, 100}}, 
 		{{-1, 0, 0, 0, 0, 3, 4, 6, 0, 3}, {298, 2187, 5054, 266, 1989, 6499, 5450, 2205, 5893, 8095}}, 
 		{{-1, 0, 1, 2, 1, 0, 5, 2, 0, 0}, {8475, 6038, 8072, 7298, 5363, 9732, 3786, 5521, 8295, 6186}},
